9116436a663f86eeda47693f4475e6037a2d7389
chiranjibi-kusari <chiranjibikusari1@gmail.com>
2024-06-30 21:57:28 +0545

    footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 <>
 <section className='bg-black w-full text-white p-3 flex justify-center items-center gap-4'>
   <FaRegCopyright className='size-6 text-white'/>
-  <h1 className='text-white text-center font-semibold'>Copyright 2024,FLEX HUB, All Rights Reserved</h1>
+  <h1 className='text-white text-center font-semibold'>Copyright 2024,FLEX HUB, All Rights Reserved Chiranjibi Kusari</h1>
 </section>
 
 <div id='icon-box' className='bg-blue-600 text-black rounded-full p-4 hover:bg-white cursor-pointer fixed lg:bottom-6 right-6 bottom-6'>
